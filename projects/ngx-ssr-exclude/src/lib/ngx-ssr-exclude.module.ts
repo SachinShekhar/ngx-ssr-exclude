34c559b11e38a8a972628f9e0d0cc8aded09329b
Sachin Shekhar <noreply.git@sachinshekhar.com>
2020-03-02 13:26:51 +0530

    Created 'browserExclude' structural directive..

--- projects/ngx-ssr-exclude/src/lib/ngx-ssr-exclude.module.ts
+++ projects/ngx-ssr-exclude/src/lib/ngx-ssr-exclude.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SSRExcludeDirective } from './ssr-exclude.directive';
+import { BrowserExcludeDirective } from './browser-exclude.directive';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [SSRExcludeDirective],
-  exports: [SSRExcludeDirective]
+  declarations: [SSRExcludeDirective, BrowserExcludeDirective],
+  exports: [SSRExcludeDirective, BrowserExcludeDirective]
 })
 export class SSRExcludeModule {}
